1d93e3c9e8413e618b55a1020f3302fe8b3aec6c
Johan <johan@forest.host>
2022-10-03 17:39:09 +0200

    not everyone needs title

--- threadedcomments/forms.py
+++ threadedcomments/forms.py
@@ -8,18 +8,11 @@ from .models import ThreadedComment
 
 
 class ThreadedCommentForm(CommentForm):
-    title = forms.CharField(label=_('Title'), required=False, max_length=getattr(settings, 'COMMENTS_TITLE_MAX_LENGTH', 255))
     parent = forms.IntegerField(required=False, widget=forms.HiddenInput)
 
     def __init__(self, target_object, parent=None, data=None, initial=None):
         # Using collections.OrderedDict from Python 2.7+
         # This class does not have an insert method, have to replace it.
-        from collections import OrderedDict
-        keys = list(self.base_fields.keys())
-        keys.remove('title')
-        keys.insert(keys.index('comment'), 'title')
-
-        self.base_fields = OrderedDict((k, self.base_fields[k]) for k in keys)
         self.parent = parent
         if initial is None:
             initial = {}
@@ -32,5 +25,4 @@ class ThreadedCommentForm(CommentForm):
     def get_comment_create_data(self, *args, **kwargs):
         d = super().get_comment_create_data(*args, **kwargs)
         d['parent_id'] = self.cleaned_data['parent']
-        d['title'] = self.cleaned_data.get('title', '')  # title can be removed
         return d
