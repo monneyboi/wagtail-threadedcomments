741fd1c9c58e76185bd5b0a14518a493de39a405
Johan <johan@forest.host>
2022-10-04 00:04:56 +0200

    add some search fields and filter

--- threadedcomments/admin.py
+++ threadedcomments/admin.py
@@ -1,6 +1,7 @@
 
 from threadedcomments.models import ThreadedComment
 from django.utils.translation import gettext_lazy as _
+from django.db.models import Count
 
 from wagtail.contrib.modeladmin.options import (
     ModelAdmin,
@@ -16,9 +17,28 @@ class CommentAdmin(ModelAdmin):
         'comment',
         'content_object',
         'user',
-        'flags',
+        'flags_count',
+        'submit_date',
     )
+    list_filter = (
+        'is_public',
+        'is_removed',
+    )
+    search_fields = (
+        'user__first_name',
+        'user__last_name',
+        'comment'
+    )
+
+    def flags_count(self, inst):
+        return inst.flags_count
+    flags_count.admin_order_field = 'flags_count'
     
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.annotate(flags_count=Count('flags'))
+
+
 # When using a ModelAdminGroup class to group several ModelAdmin classes together,
 # you only need to register the ModelAdminGroup class with Wagtail:
 modeladmin_register(CommentAdmin)
